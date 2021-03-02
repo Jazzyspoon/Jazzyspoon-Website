31266f919c38c317bb53b8ef4acff2a5a7140c16
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-03-01 22:54:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jazzyspoon-Website
+ Working on a new version of my music website for 2021.  Using html, css, bootstrap, jquery, and javascript.
---
+++ js/scripts.js
@@ -0,0 +1,94 @@
+window.onbeforeunload = () => {
+  window.scrollTo(0, 0);
+};
+
+$(function () {
+    //replaces images with the pretty thumbnails
+  $(".imageLink").thumbs();
+});
+
+window.scroll({
+  top: 0, 
+  left: 0, 
+  behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({ 
+  top: 0, // could be negative value
+  left: 0, 
+  behavior: 'smooth' 
+});
+
+// Scroll to a certain element
+document.querySelector('.hello').scrollIntoView({ 
+  behavior: 'smooth' 
+});
+
+//modal1
+$(function() {
+  $(".video").click(function () {
+  let theModal = $(this).data("target"),
+  videoSRC = $(this).attr("data-video"),
+  videoSRCauto = videoSRC;
+  $(theModal + ' iframe').attr('src', videoSRCauto);
+  $(theModal + ' button.close').click(function () {
+      $(theModal + ' iframe').attr('src', '');
+  });
+  });
+});
+
+$('#videoModal').on('hidden.bs.modal', function (e) {
+  $('#videoModal').find('iframe').attr('src', '');
+  });
+
+  //modal2
+$(function() {
+  $(".video2").click(function () {
+  let theModal = $(this).data("target"),
+  videoSRC = $(this).attr("data-video"),
+  videoSRCauto = videoSRC;
+  $(theModal + ' iframe').attr('src', videoSRCauto);
+  $(theModal + ' button.close').click(function () {
+      $(theModal + ' iframe').attr('src', '');
+  });
+  });
+});
+
+$('#videoModal2').on('hidden.bs.modal', function (e) {
+  $('#videoModal2').find('iframe').attr('src', '');
+  });
+
+    //modal3
+$(function() {
+  $(".video3").click(function () {
+  let theModal = $(this).data("target"),
+  videoSRC = $(this).attr("data-video"),
+  videoSRCauto = videoSRC;
+  $(theModal + ' iframe').attr('src', videoSRCauto);
+  $(theModal + ' button.close').click(function () {
+      $(theModal + ' iframe').attr('src', '');
+  });
+  });
+});
+
+$('#videoModal3').on('hidden.bs.modal', function (e) {
+  $('#videoModal3').find('iframe').attr('src', '');
+  });
+
+     //modal3
+$(function() {
+  $(".video4").click(function () {
+  let theModal = $(this).data("target"),
+  videoSRC = $(this).attr("data-video"),
+  videoSRCauto = videoSRC;
+  $(theModal + ' iframe').attr('src', videoSRCauto);
+  $(theModal + ' button.close').click(function () {
+      $(theModal + ' iframe').attr('src', '');
+  });
+  });
+});
+
+$('#videoModal4').on('hidden.bs.modal', function (e) {
+  $('#videoModal4').find('iframe').attr('src', '');
+  });
