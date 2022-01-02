8fa9747ae93525a9c160b9c72c4c495c5376d281
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2022-01-02 11:37:46 -0700

    updating some links and files

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,14 @@ window.onbeforeunload = () => {
 window.scroll({
   top: 0,
   left: 0,
-  behavior: "smooth",
+  behavior: "auto",
 });
 
 // Scroll certain amounts from current position
 window.scrollBy({
   top: 0,
   left: 0,
-  behavior: "smooth",
+  behavior: "auto",
 });
 
 $(function () {
