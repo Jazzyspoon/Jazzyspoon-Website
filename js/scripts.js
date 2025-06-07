07f4b9523a7ee49928542f61f224e74ea34d8466
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2025-06-07 14:38:11 -0500

    updates and optimizations

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,24 @@
-//load site at top of scroll page
-window.onbeforeunload = () => {
-  window.scrollTo(0, 0);
-};
-
-window.scroll({
-  top: 0,
-  left: 0,
-  behavior: "auto",
-});
-
-// Scroll certain amounts from current position
-window.scrollBy({
-  top: 0,
-  left: 0,
-  behavior: "auto",
+// Simplify scroll behavior
+document.addEventListener('DOMContentLoaded', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'auto'
+  });
 });
 
 $(function () {
-  $(".video").click(function () {
+  $(".video, .video2, .video3, .video4").click(function () {
     let theModal = $(this).data("target"),
       videoSRC = $(this).attr("data-video"),
       videoSRCauto = videoSRC;
     $(theModal + " iframe").attr("src", videoSRCauto);
-    $(theModal + " button.close").click(function () {
-      $(theModal + " iframe").attr("src", "");
-    });
   });
-});
-
-$("#videoModal").on("hidden.bs.modal", function (e) {
-  $("#videoModal").find("iframe").attr("src", "");
-});
-
-//modal2
-$(function () {
-  $(".video2").click(function () {
-    let theModal = $(this).data("target"),
-      videoSRC = $(this).attr("data-video"),
-      videoSRCauto = videoSRC;
-    $(theModal + " iframe").attr("src", videoSRCauto);
-    $(theModal + " button.close").click(function () {
-      $(theModal + " iframe").attr("src", "");
-    });
+  
+  // Handle all video modals with a single event handler
+  $(".modal").on("hidden.bs.modal", function (e) {
+    $(this).find("iframe").attr("src", "");
   });
-});
-
-$("#videoModal2").on("hidden.bs.modal", function (e) {
-  $("#videoModal2").find("iframe").attr("src", "");
-});
-
-//modal3
-$(function () {
-  $(".video3").click(function () {
-    let theModal = $(this).data("target"),
-      videoSRC = $(this).attr("data-video"),
-      videoSRCauto = videoSRC;
-    $(theModal + " iframe").attr("src", videoSRCauto);
-    $(theModal + " button.close").click(function () {
-      $(theModal + " iframe").attr("src", "");
-    });
-  });
-});
-
-$("#videoModal3").on("hidden.bs.modal", function (e) {
-  $("#videoModal3").find("iframe").attr("src", "");
-});
-
-//modal4
-$(function () {
-  $(".video4").click(function () {
-    let theModal = $(this).data("target"),
-      videoSRC = $(this).attr("data-video"),
-      videoSRCauto = videoSRC;
-    $(theModal + " iframe").attr("src", videoSRCauto);
-    $(theModal + " button.close").click(function () {
-      $(theModal + " iframe").attr("src", "");
-    });
-  });
-});
-
-$("#videoModal4").on("hidden.bs.modal", function (e) {
-  $("#videoModal4").find("iframe").attr("src", "");
 });
 
 //activating the email link
@@ -94,13 +32,3 @@ $(".navbar-nav>li").on("click", function () {
   $(".navbar-collapse").collapse("hide");
 });
 
-// $(".img-fluid").addClass("wow fadeIn z-depth-1-half");
-
-// new WOW().init();
-
-// var tooltipTriggerList = [].slice.call(
-//   document.querySelectorAll('[data-bs-toggle="tooltip"]')
-// );
-// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-//   return new bootstrap.Tooltip(tooltipTriggerEl);
-// });
